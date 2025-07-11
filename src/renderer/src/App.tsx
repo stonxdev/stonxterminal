c16dbb95f6792bb0eeb804d1ff31e0a16d0b7470
Andre Pena <andrerpena@gmail.com>
2025-07-11 13:36:21 +0100

    nice

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,30 +1,20 @@
-import {
-  Dockable,
-  useDockableLocalStorage,
-} from "@renderer/components/dockable";
+// import { useDockableLocalStorage } from "@renderer/components/dockable";
 import "./App.css";
 
 export const App: React.FC = () => {
-  const { layout, setLayout } = useDockableLocalStorage(3);
-
-  const viewCount = 2;
-  const views = Array.from({ length: viewCount }, (_, i) =>
-    createView(`view-${i + 1}`, `View ${i + 1}`),
-  );
+  // const { layout, setLayout } = useDockableLocalStorage(2);
 
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
-      <Dockable.Root layout={layout} onChange={setLayout}>
-        {views}
-      </Dockable.Root>
+      Hello
+      {/* <Dockable.Root layout={layout} onChange={setLayout}>
+        <Dockable.Tab id={"1"} name={"View 1"}>
+          {"testing content"}
+        </Dockable.Tab>
+        <Dockable.Tab id={"2"} name={"View 2"}>
+          {"testing content"}
+        </Dockable.Tab>
+      </Dockable.Root> */}
     </div>
   );
 };
-
-function createView(id: string, name: string) {
-  return (
-    <Dockable.Tab id={id} name={name}>
-      {"testing content"}
-    </Dockable.Tab>
-  );
-}
