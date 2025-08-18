0dc97372ef9de56918c48e36c00f4e27c0fa8535
Andre Pena <andrerpena@gmail.com>
2025-08-18 09:00:56 +0100

    wip

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -5,17 +5,15 @@ import {
   LeftPanel,
   RightPanel,
 } from "@renderer/components/layout/panels-old/DemoPanels";
+import PixiSquare from "./components/PixiExample";
 
 export const App: React.FC = () => {
   return (
     <div style={{ width: "100vw", height: "100vh", overflow: "auto" }}>
       <DockSystem
-        leftTop={<LeftPanel />}
-        leftBottom={<LeftPanel />}
-        center={<CenterPanel />}
-        centerBottom={<BottomPanel />}
-        rightTop={<RightPanel />}
-        rightBottom={<RightPanel />}
+        leftTop={<div>Left</div>}
+        center={<PixiSquare />}
+        rightTop={<div>Right</div>}
       />
     </div>
   );
